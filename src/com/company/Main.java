0ba900fc45023953b58a259c33428d420af398ad
markus-sch <markusschlussmeier@gmail.com>
2021-04-16 07:49:06 +0200

    Hypothenusen-Problem mit Math.pow umgesetzt.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,9 @@ public class Main {
         double b = 4;
         double h = 0;
 
-        h = Math.sqrt(a * a + b * b);
+        // h = Math.sqrt(a * a + b * b);
+        // Die vorherige Formel kann man auch durch Math.pow ersetzen.
+        h = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
         System.out.println("Die Hypothenuse ist: " + h);
 
     }
