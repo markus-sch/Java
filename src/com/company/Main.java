8a7c684ca57d1ff897892523872ab0f11716df7a
markus-sch <markusschlussmeier@gmail.com>
2021-04-16 08:31:19 +0200

    Obtaining the Minimum of Two Numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         double a = 3;
         double b = 4;
-        double h = 0;
+        double m = 0;
+        String r = "The minimum  of numbers: ";
 
-        // h = Math.sqrt(a * a + b * b);
-        // Die vorherige Formel kann man auch durch Math.pow ersetzen.
-        h = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
-        System.out.println("Die Hypothenuse ist: " + h);
+        m = Math.min(a, b);
+        System.out.println(r + a + " and " + b + " is "  + m);
+
 
     }
 }
