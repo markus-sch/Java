2039310e2fda979ef06ca7a545fe4631fcf05844
markus-sch <markusschlussmeier@gmail.com>
2021-04-16 07:33:34 +0200

    Erste Programme vor Löschen, um das Trigonometry-Problem zu lösen

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        String t = "This is a text";
+        char c ='A';
+        int i = 53;
+        float f = 23.08f;
+        double d = 1.97;
+        boolean b = true;
+        String j = null;
+        System.out.println(j);
+
+        System.out.println(t);
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println("This is text");
+        System.out.println('A');
+        System.out.println(53);
+        System.out.println(23.08f);
+        System.out.println(1.97);
+        System.out.println(true);
+        j = "Joe ";
+        j = j + "went fishing";
+        System.out.println(j);
+    }
+}
