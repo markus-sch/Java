df37f14ed7e109565c3df6a5767ce2858568ee50
markus-sch <markusschlussmeier@gmail.com>
2021-04-16 07:43:56 +0200

    Hypothenusen-Problem mit sqrt gerechnet.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,29 +2,12 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        String t = "This is a text";
-        char c ='A';
-        int i = 53;
-        float f = 23.08f;
-        double d = 1.97;
-        boolean b = true;
-        String j = null;
-        System.out.println(j);
+        double a = 3;
+        double b = 4;
+        double h = 0;
 
-        System.out.println(t);
-        System.out.println(c);
-        System.out.println(i);
-        System.out.println(f);
-        System.out.println(d);
-        System.out.println(b);
-        System.out.println("This is text");
-        System.out.println('A');
-        System.out.println(53);
-        System.out.println(23.08f);
-        System.out.println(1.97);
-        System.out.println(true);
-        j = "Joe ";
-        j = j + "went fishing";
-        System.out.println(j);
+        h = Math.sqrt(a * a + b * b);
+        System.out.println("Die Hypothenuse ist: " + h);
+
     }
 }
